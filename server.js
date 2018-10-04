ecfb7bf70f258353036634f883f2c4aa67b8069f
ColmanZhu <cozhu2@gmail.com>
2018-10-03 19:29:45 -0700

    Colman test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const user = require('./routes/api/user');
 const profile = require('./routes/api/profile');
 
 const app = express();
-
+//Test
 //Database Config
 let db = mysql.createConnection({
     host: 'localhost',
