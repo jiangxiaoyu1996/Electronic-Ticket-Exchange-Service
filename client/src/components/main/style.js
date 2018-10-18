337b8b25864670d940bf597d2debf91158dc06cd
Charlene Jiang <jiangxiaoyu1996@gmail.com>
2018-10-17 17:09:53 -0700

    Before demo

---
+++ client/src/components/main/style.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const styles = {
+    card: {
+        width: "100%",
+        height: "100%"
+    },
+    image: {
+        height: "-webkit-fill-available",
+    },
+};
