9221e941389a43329ab8604b7b12a123198fcabb
Colman Zhu <cozhu2@gmail.com>
2018-10-24 07:44:28 -0700

    Populate db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const async = require('async');
 
 
 //web pages
@@ -18,34 +19,56 @@ function getMySQLConnection() {
     });
 }
 
-async function deleteDb(){
-	connection = getMySQLConnection();
-	connection.query('DROP TABLE IF EXISTS event', function(err, rows, fields){
+function deleteDb(){
+	var connection = getMySQLConnection();
+	connection.query('DROP TABLE IF EXISTS event, user, ticket', function(err, rows, fields){
+		createDb();
+	});
+	connection.end()
+}
+
+function createDb(){
+	var connection = getMySQLConnection();
+	connection.query('CREATE TABLE event (event_name VARCHAR(255), event_ID VARCHAR(255), date VARCHAR(255), location VARCHAR(255), ticket_amount INTEGER, max_rows INTEGER, max_cols INTEGER, description TEXT, PRIMARY KEY (event_name, event_ID))', function(err, rows, fields){
+		connection.query('CREATE TABLE user (id VARCHAR(255), email TEXT, password TEXT, PRIMARY KEY (id))', function(err, rows, fields){
+			connection.query('CREATE TABLE ticket (id VARCHAR(255), event VARCHAR(255), row_Number INTEGER, col_Number INTEGER, buyer TEXT, seller TEXT, PRIMARY KEY (id, event, row_Number, col_Number))', function(err, rows, fields){
+				populateDb();
+			});
+		});;
 	});
 }
 
-async function createDb(){
-	connection = getMySQLConnection();
-	connection.query('CREATE TABLE IF NOT EXISTS event (event_name VARCHAR(255), event_ID VARCHAR(255), dat VARCHAR(255), location VARCHAR(255), ticket_amount INTEGER, max_rows INTEGER, max_cols INTEGER, description TEXT, PRIMARY KEY (event_name, event_ID))', function(err, rows, fields){
+
+
+function populateDb(){
+	var connection = getMySQLConnection();
+	var lock = 3
+	connection.query('INSERT INTO event (event_name, event_ID, date, location, ticket_amount, max_rows, max_cols, description) VALUES (' +
+	mysql.escape('Bay Area Taco & Beer Festival') + ', 1000, ' + mysql.escape('11/02/2018') + ', ' + mysql.escape('San Jose,CA') + ', 500, NULL, NULL, ' + mysql.escape('Guests will experience unlimited pours of craft beer from local and regional breweries as well as unlimited taco tastings from some of the top restaurants & food trucks in California. In addition to the free unlimited taco tastings, restaurants & food trucks will be selling additional food and beverage items. While sampling beer and tacos, make sure to stop by our boutique vendors where you can purchase orginal art. There will also be DJs, games, and more!)') +
+	'), (' + mysql.escape('Los Angeles Lakers vs Golden State Warriors') + ', 1001, ' + mysql.escape('12/25/2018') + ', ' + mysql.escape('Oakland,CA') + ', 1000, 100, 20, ' + mysql.escape('The Golden State Warriors and Los Angeles Lakers are two of the most fun teams to watch in the NBA, and it is always a great game when the two match up. Exciting action on the court, some of the best athletes and players in the world, and an energetic fanbase: these are the things you can count on when the Warriors and Lakers go toe to toe. You can expect a great time at the arena during Warriors vs. Lakers games.') +
+	'), (' + mysql.escape('Magnificent Mozart') + ', 1002, ' + mysql.escape('11/03/2018') + ', ' + mysql.escape('Santa Clara,CA') + ', 200, 20, 20, ' + mysql.escape('Mission College Chorus joins Mission College Symphony for a spectacular evening of Mozart, set in the beautiful Mission Santa Clara church at Santa Clara University.') + ')', function(err, rows, fields){
+	lock -= 1;
 	});
+	connection.query('INSERT INTO user (id, email, password) VALUES (1, ' + "'test@gmail.com'" + ', 123), (2, ' + "'user1@gmail.com'" + ', password), (500, ' + "'another1@gmail.com'" + ', ' + "'abc123456'" + ')', function(err, rows, fields){
+	lock -= 1;
+	});
+	connection.query('INSERT INTO ticket (id, event, row_Number, col_Number, buyer, seller) VALUES (1, ' + mysql.escape('Bay Area Taco & Beer Festival') + ', 1, 1, NULL, ' + "'test@gmail.com'" +
+	'), (1, ' + mysql.escape('Bay Area Taco & Beer Festival') + ', 20, 15, ' + "'another1@gmail.com'" + ', ' + "'user1@gmail.com'" + ')' , function(err, rows, fields){
+	if(err){
+		console.log(err)
+	}
+	lock -= 1;
+	});
+	if(lock == 0){
+		connection.end()
+	}
 }
 
-async function populateDB(){
-	connection = getMySQLConnection();
-	connection.query('INSERT INTO event (event_name VARCHAR(255), event_ID VARCHAR(255), date VARCHAR(255), location VARCHAR(255), ticket_amount INTEGER, max_rows INTEGER, max_cols INTEGER, description TEXT) VALUES' +
-	'(Bay Area Taco & Beer Festival, 1000, 11/02/2018, San Jose,CA, 500, NULL, NULL, Guests will experience unlimited pours of craft beer from local and regional breweries as well as unlimited taco tastings from some of the top restaurants & food trucks in California. In addition to the free unlimited taco tastings, restaurants & food trucks will be selling additional food and beverage items. While sampling beer and tacos, make sure to stop by our boutique vendors where you can purchase orginal art. There will also be DJ\s, games, and more!),' +
-	'(Los Angeles Lakers vs Golden State Warriors\r\n, 1001, 12/25/2018, Oakland,CA, 1000, 100, 20, The Golden State Warriors and Los Angeles Lakers are two of the most fun teams to watch in the NBA, and it is always a great game when the two match up. Exciting action on the court, some of the best athletes and players in the world, and an energetic fanbase: these are the things you can count on when the Warriors and Lakers go toe to toe. You can expect a great time at the arena during Warriors vs. Lakers games.),' +
-	'(Magnificent Mozart, 1002, 11/03/2018, Santa Clara,CA, 200, 20, 20, Mission College Chorus joins Mission College Symphony for a spectacular evening of Mozart, set in the beautiful Mission Santa Clara church at Santa Clara University.)', function(err, rows, fields){
-	});
-}
-
-function setupDB(){
+function setupDb(){
 	deleteDb();
-	createDb();
 }
 
-setupDB();
-
+setupDb();
 
 app.get('/', (req, res) => res.send('Hello there!'));
 //Use Routes
