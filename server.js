59782cb95c0b72b8139363a675ae6bff7e1d8ef7
Codyyu36 <36931240+Codyyu36@users.noreply.github.com>
2018-10-23 17:01:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,10 @@ async function createDb(){
 
 async function populateDB(){
 	connection = getMySQLConnection();
-	connection.query('', function(err, rows, fields){
+	connection.query('INSERT INTO `event` (`event_name`, `event_ID`, `date`, `location`, `ticket_amount`, `MAX_ROWS`, `max_cols`, `description`) VALUES
+('Bay Area Taco & Beer Festival', '1000', '11/02/2018', 'San Jose,CA', 500, NULL, NULL, 'Guests will experience unlimited pours of craft beer from local and regional breweries as well as unlimited taco tastings from some of the top restaurants & food trucks in California.\r\nIn addition to the free unlimited taco tastings, restaurants & food trucks will be selling additional food and beverage items.\r\nWhile sampling beer and tacos, make sure to stop by our boutique vendors where you can purchase orginal art. There will also be DJ\'s, games, and more!'),
+('Los Angeles Lakers vs Golden State Warriors\r\n', '1001', '12/25/2018', 'Oakland,CA', 1000, 100, 20, 'The Golden State Warriors and Los Angeles Lakers are two of the most fun teams to watch in the NBA, and it is always a great game when the two match up. Exciting action on the court, some of the best athletes and players in the world, and an energetic fanbase: these are the things you can count on when the Warriors and Lakers go toe to toe. You can expect a great time at the arena during Warriors vs. Lakers games.'),
+('Magnificent Mozart', '1002', '11/03/2018', 'Santa Clara,CA', 200, 20, 20, 'Mission College Chorus joins Mission College Symphony for a spectacular evening of Mozart, set in the beautiful Mission Santa Clara church at Santa Clara University.')', function(err, rows, fields){
 	});
 }
 
