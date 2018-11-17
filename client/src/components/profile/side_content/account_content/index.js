1494bc2f914242e15d8e54d41b7b7f31875dbee8
Charlene Jiang <jiangxiaoyu1996@gmail.com>
2018-11-16 23:22:11 -0800

    add username and address to profile

--- client/src/components/profile/side_content/account_content/index.js
+++ client/src/components/profile/side_content/account_content/index.js
@@ -4,19 +4,67 @@ import {withStyles} from "@material-ui/core/styles/index";
 import Typography from "@material-ui/core/es/Typography/Typography";
 
 import {styles} from "../styles";
+import TextField from "@material-ui/core/es/TextField/TextField";
+import Grid from "@material-ui/core/es/Grid/Grid";
 
 class AccountContent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            address: '',
+            username: ''
+        }
+    }
+
     render(){
         const { classes } = this.props;
 
+        //console.log(this.state.address);
         return (
             <div className={classes.content}>
-                <Typography style={{margin: 20}}>
-                    Email: {this.props.userInfo.email}
-                </Typography>
-                <Typography style={{margin: 20}}>
-                    Address: {this.props.userInfo.address}
-                </Typography>
+                <Grid container justify="center" spacing={256}>
+                    <Grid item xs={12}>
+                        <Typography>
+                            Email
+                        </Typography>
+                        <TextField
+                            id="outlined-read-only-input"
+                            value={this.props.userInfo.email}
+                            className={classes.textField}
+                            margin="normal"
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography>
+                            Username
+                        </Typography>
+                        <TextField
+                            id="outlined-name"
+                            className={classes.textField}
+                            value={this.props.userInfo.username}
+                            margin="normal"
+                            variant="outlined"
+                            onChange = {(event) => this.setState({username: event.target.value})}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography>
+                            Address
+                        </Typography>
+                        <TextField
+                            id="outlined-name"
+                            className={classes.textField}
+                            value={this.props.userInfo.address}
+                            margin="normal"
+                            variant="outlined"
+                            onChange = {(event) => this.setState({address: event.target.value})}
+                        />
+                    </Grid>
+                </Grid>
             </div>
         );
     }
